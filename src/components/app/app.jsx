60e0e4b0b9a0398f8638638040445a3b6bd7ea7a
Akzhar <akzharik@mail.ru>
2021-02-07 20:06:50 +0300

    Добавляет компоненты страниц, настраивает базовую маршрутизацию

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {offerShape} from '../../prop-types/prop-types.jsx';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import MainScreen from '../main-screen/main-screen.jsx';
+import LoginScreen from '../login-screen/login-screen.jsx';
+import FavoritesScreen from '../favorites-screen/favorites-screen.jsx';
+import RoomScreen from '../room-screen/room-screen.jsx';
+import NotFoundScreen from '../not-found-screen/not-found-screen.jsx';
 
 const App = ({offers}) => (
-  <MainScreen offers={offers}/>
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" exact>
+        <MainScreen offers={offers}/>
+      </Route>
+      <Route path="/login" exact>
+        <LoginScreen/>
+      </Route>
+      <Route path="/favorites" exact>
+        <FavoritesScreen/>
+      </Route>
+      <Route path="/room/:id" exact component={RoomScreen}/>
+      <Route>
+        <NotFoundScreen/>
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 App.propTypes = {
