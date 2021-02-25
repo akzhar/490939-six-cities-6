469e8de37b6dd33fae7e50aab94720515dc3c301
Akzhar <akzharik@mail.ru>
2021-02-26 01:26:29 +0300

    Создает store с помощью redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './store/reducer.js';
 
 import offers from './mocks/offers.json';
 import reviews from './mocks/reviews.json';
 
+const store = createStore(reducer, composeWithDevTools());
+
 ReactDOM.render(
-    <App
-      offers={offers}
-      reviews={reviews}
-    />,
+    <Provider store={store}>
+      <App
+        offers={offers}
+        reviews={reviews}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
