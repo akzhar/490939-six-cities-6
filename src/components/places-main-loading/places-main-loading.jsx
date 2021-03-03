6f94f7190224abea8cc243a829c46f4dbd776eb2
Akzhar <akzharik@mail.ru>
2021-03-03 23:43:47 +0300

    Исправляет опечатку

--- src/components/places-main-loading/places-main-loading.jsx
+++ src/components/places-main-loading/places-main-loading.jsx
@@ -4,7 +4,7 @@ const PlacesMainLoading = () => (
   <section className="cities__no-places">
     <div className="cities__status-wrapper tabs__content">
       <b className="cities__status">Loading offers for you...</b>
-      <p className="cities__status-description">Please wait few seconds...</p>
+      <p className="cities__status-description">Please wait a few seconds...</p>
     </div>
   </section>
 );
